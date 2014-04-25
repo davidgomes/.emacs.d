37a93dd4541cd7dfc8c3ac354cd49c0bd37684ba
David Gomes <davidrafagomes@gmail.com>
2014-04-25 20:48:26 +0100

    Fixes scroll bell on OS X. Changes my default font.

--- init.el
+++ init.el
@@ -149,7 +149,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "xos4" :slant normal :weight normal :height 130 :width normal)))))
+ '(default ((t (:family "Cousine" :foundry "xos4" :slant normal :weight normal :height 130 :width normal)))))
 
 (load-theme 'wombat t)
 
@@ -256,3 +256,6 @@
 (setq mac-option-modifier nil
       mac-command-modifier 'meta
       x-select-enable-clipboard t)
+
+;; Another fix for Emacs on OS X, stop the beeping on scrolling
+(setq ring-bell-function #'ignore)
