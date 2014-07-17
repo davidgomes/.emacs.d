8945f759070619279eb156ad029f21a41f138632
David Gomes <davidrafagomes@gmail.com>
2014-07-17 13:15:24 +0100

    Bigger font.

--- init.el
+++ init.el
@@ -151,7 +151,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cousine" :foundry "xos4" :slant normal :weight normal :height 130 :width normal)))))
+ '(default ((t (:family "Inconsolata" :foundry "xos4" :slant normal :weight normal :height 170 :width normal)))))
 
 (load-theme 'wombat t)
 
