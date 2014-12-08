efdd20c4f790ea43800bddb580727f1255fa29c8
David Gomes <davidrafagomes@gmail.com>
2014-12-08 18:27:59 +0000

    Changes my font do Droid Sans Mono.

--- init.el
+++ init.el
@@ -148,7 +148,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "xos4" :slant normal :weight normal :height 200 :width normal)))))
+ '(default ((t (:family "Droid Sans Mono" :foundry "xos4" :slant normal :weight normal :height 170 :width normal)))))
 
 (load-theme 'wombat t)
 
