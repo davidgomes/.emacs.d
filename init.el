005113dc9029840a6360dd0717cbdd3932e3f837
David Gomes <gomes@memsql.com>
2018-10-14 17:41:49 +0100

    clean up

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 
 ;; Add some package repositories
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ;("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 
@@ -12,18 +12,17 @@
 
 ;; List all packages
 (setq required-packages
-      '(nyan-mode auto-complete popup crosshairs vline col-highlight vline hl-line+ django-mode elpy yasnippet pyvenv highlight-indentation find-file-in-project company f dash s find-file-in-project fuzzy gist gh logito pcache go-mode handlebars-mode haskell-mode helm-flycheck helm async flycheck let-alist pkg-info epl dash dash highlight-indentation hl-line+ idomenu iedit jabber jinja2-mode js2-mode json-mode json-snatcher json-reformat json-reformat json-snatcher jsx-mode less-css-mode let-alist logito lua-mode magit magit-popup dash git-commit with-editor dash dash with-editor dash magit-popup dash moe-theme nav nose paredit pcache pkg-info epl popup powerline processing-mode pyvenv quack s sass-mode haml-mode scheme-complete scss-mode solarized-theme dash vimrc-mode vline web-mode with-editor dash yasnippet noctilux-theme helm-projectile neotree))
+      '(nyan-mode auto-complete popup vline col-highlight vline hl-line+ elpy pyvenv highlight-indentation find-file-in-project company f dash s find-file-in-project fuzzy gist gh logito pcache go-mode handlebars-mode haskell-mode helm-flycheck helm async flycheck let-alist pkg-info epl dash dash highlight-indentation hl-line+ idomenu iedit jabber jinja2-mode js2-mode json-mode json-snatcher json-reformat json-reformat json-snatcher jsx-mode less-css-mode let-alist logito lua-mode magit magit-popup dash git-commit with-editor dash dash with-editor dash magit-popup dash moe-theme nav nose paredit pcache pkg-info epl popup powerline processing-mode pyvenv quack s sass-mode haml-mode scheme-complete scss-mode solarized-theme dash vimrc-mode vline web-mode with-editor dash noctilux-theme helm-projectile neotree))
+
+;; Fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
 
 ;; Installs missing packages
 (defun install-missing-packages ()
-  "Installs required packages that are missing"
-  (interactive)
-  (package-refresh-contents)
-  (mapc (lambda (package)
-          (unless (package-installed-p package)
-            (package-install package)))
-        required-packages)
-  (message "Installed all missing packages!"))
+  (dolist (package required-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
 
 ;; Load plugins
 (add-to-list 'load-path "~/.emacs.d/lisp/")
@@ -47,7 +46,7 @@
 (column-number-mode 1)
 (show-paren-mode 1)
 (size-indication-mode 1)
-(menu-bar-mode 1)
+(menu-bar-mode 0)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 (setq ns-use-native-fullscreen nil)
@@ -92,21 +91,15 @@
  '(custom-safe-themes
    (quote
     ("2b2188dc6e921b700377f408990741a1dd6d37307bccc9e451dbfa957d5378e1" "83c5800f7946a3a996dfd46860076e3878184adc412b78162b6e293c791b7be5" "6dbeb8cf8dccee0e5ff5699713b8b19a1d28c5c4a4a510ca56322ba81ef510f2" "3ff96689086ebc06f5f813a804f7114195b7c703ed2f19b51e10026723711e33" "ba9be9caf9aa91eb34cf11ad9e8c61e54db68d2d474f99a52ba7e87097fa27f5" "8aa7eb0cc23931423f719e8b03eb14c4f61aa491e5377073d6a55cba6a7bc125" "d3501680958e5ab7e641c70050576fbd72ac5902eaba370fae9c35925c319b46" "3b0a350918ee819dca209cec62d867678d7dac74f6195f5e3799aa206358a983" "579e9950513524d8739e08eae289419cfcb64ed9b7cc910dd2e66151c77975c4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "0c311fb22e6197daba9123f43da98f273d2bfaeeaeb653007ad1ee77f0003037" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "6ace5b419f7997a7879ba299aa55ca783b5d60e505d7364ee358be4e94c6c73d" "1157a4055504672be1df1232bed784ba575c60ab44d8e6c7b3800ae76b42f8bd" "7f1263c969f04a8e58f9441f4ba4d7fb1302243355cb9faecb55aec878a06ee9" "8afd01e30f62250645c6d68dce8e3581b606aad57292ea88c3ae1bd37fc29285" "569dc84822fc0ac6025f50df56eeee0843bffdeceff2c1f1d3b87d4f7d9fa661" "73fe242ddbaf2b985689e6ec12e29fab2ecd59f765453ad0e93bc502e6e478d6" "f0ea6118d1414b24c2e4babdc8e252707727e7b4ff2e791129f240a2b3093e32" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "18d91d95e20450b0cdab4d7eed600e80c22cc7a4153a87989daa5a1c5aff3b83" "99cbc2aaa2b77374c2c06091494bd9d2ebfe6dc5f64c7ccdb36c083aff892f7d" "2affb26fb9a1b9325f05f4233d08ccbba7ec6e0c99c64681895219f964aac7af" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "09c2fd812c3c046379d84beb4795db273da1fe84b008dfbb4c03f54a10cf7f0e" "c1f0d3ec9563620ede55e0631ed3e959bcb619d5276b546f4ca1534f5c8db450" "ac69b7e2e928dc1560d5a556043c99d8cb0614c30957bd03dfe82be4a9e917ee" default)))
- '(js-indent-level 4 t)
+ '(horizontal-scroll-bar-mode nil)
+ '(js-indent-level 4)
  '(js2-basic-offset 4)
  '(js2-bounce-indent-p t)
  '(js2-indent-switch-body t t)
  '(js2-strict-trailing-comma-warning nil)
  '(package-selected-packages
    (quote
-    (reason-mode caml graphql-mode neotree noctilux-theme web-mode vimrc-mode solarized-theme scss-mode scheme-complete sass-mode quack processing-mode powerline paredit nose nav moe-theme magit lua-mode less-css-mode json-mode jinja2-mode jabber iedit idomenu helm-flycheck haskell-mode handlebars-mode go-mode auto-complete jsx-mode helm-projectile js2-mode nyan-mode helm gist fuzzy f elpy django-mode crosshairs clojure-mode magit handlebars-mode markdown-mode sunshine cider rjsx-mode)))
- '(safe-local-variable-values
-   (quote
-    ((eval when
-           (fboundp
-            (quote rainbow-mode))
-           (rainbow-mode 1)))))
- '(send-mail-function (quote mailclient-send-it)))
+    (elpy restart-emacs git-commit flycheck nyan-mode django-mode col-highlight auto-complete))))
 
 ;; HTML/Web stuff
 (add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
@@ -181,9 +174,6 @@
 (global-unset-key (kbd "C-x C-b"))
 (global-unset-key (kbd "C-x C-z"))
 (global-unset-key (kbd "C-x o"))
-(global-unset-key (kbd "C-x 0"))
-(global-unset-key (kbd "C-x 2"))
-(global-unset-key (kbd "C-x 3"))
 (global-unset-key (kbd "<f10>"))
 (global-unset-key (kbd "C-w"))
 (global-unset-key (kbd "C-t"))
@@ -199,32 +189,22 @@
 ;; (global-set-key (kbd "<f11>") 'toggle-fullscreen)
 (global-set-key (kbd "<C-tab>") 'next-multiframe-window)
 (global-set-key (kbd "<C-S-iso-lefttab>") 'previous-multiframe-window)
-(global-set-key (kbd "s-0") 'delete-window)
-(global-set-key (kbd "s-2") 'split-window-below)
-(global-set-key (kbd "s-3") 'split-window-right)
+(global-set-key (kbd "C-x 0") 'delete-window)
+(global-set-key (kbd "C-x 2") 'split-window-below)
+(global-set-key (kbd "C-x 3") 'split-window-right)
 (global-set-key (kbd "<f12>") 'delete-trailing-whitespace)
 (global-set-key (kbd "<f10>") 'fix-indentation)
 (global-set-key (kbd "C-h") 'replace-string)
-(global-set-key (kbd "s-t") 'helm-projectile)
-(global-set-key (kbd "s-T") 'helm-for-files)
-(global-set-key (kbd "s-b") 'helm-mini)
+(global-set-key (kbd "C-t") 'helm-projectile)
+(global-set-key (kbd "C-T") 'helm-for-files)
+(global-set-key (kbd "C-h") 'helm-mini)
 (global-set-key (kbd "<f9>") 'recompile)
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
 (global-set-key (kbd "C-w") 'execute-extended-command)
 (global-set-key (kbd "s-F") 'projectile-grep)
 (global-set-key (kbd "<f6>") (lambda () (interactive) (toggle-frame-fullscreen) (toggle-frame-fullscreen)))
-
-(global-set-key (kbd "C-j") 'next-line)
-(global-set-key (kbd "C-k") 'previous-line)
-(global-set-key (kbd "C-h") 'backward-char)
-(global-set-key (kbd "C-l") 'forward-char)
-
-;; TODO I have to add shift for region selection
-;; (global-set-key (kbd "C-s-j") 'next-line)
-;; (global-set-key (kbd "C-s-k") 'previous-line)
-;; (global-set-key (kbd "C-s-h") 'backward-char)
-;; (global-set-key (kbd "C-s-l") 'forward-char)
-
+(global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "s-a") 'mark-whole-buffer)
 
 ;; From http://ergoemacs.org/emacs/emacs_kill-ring.html
 (defun my-delete-word (arg)
@@ -318,7 +298,7 @@ This command does not push text to `kill-ring'."
 
 ;; List all packages
 (setq required-packages
-      '(nyan-mode auto-complete popup crosshairs vline col-highlight vline hl-line+ django-mode elpy yasnippet pyvenv highlight-indentation find-file-in-project company f dash s find-file-in-project fuzzy gccsense gist gh logito pcache go-mode handlebars-mode haskell-mode helm-flycheck helm async flycheck let-alist pkg-info epl dash dash highlight-indentation hl-line+ idomenu iedit jabber jinja2-mode js2-mode json-mode json-snatcher json-reformat json-reformat json-snatcher jsx-mode less-css-mode let-alist logito lua-mode magit magit-popup dash git-commit with-editor dash dash with-editor dash magit-popup dash moe-theme nav nose paredit pcache pkg-info epl popup powerline processing-mode pyvenv quack s sass-mode haml-mode scheme-complete scss-mode solarized-theme dash vimrc-mode vline web-mode with-editor dash yasnippet noctilux-theme helm-projectile neotree))
+      '(nyan-mode auto-complete popup django-mode elpy pyvenv highlight-indentation find-file-in-project company f dash s find-file-in-project fuzzy gccsense gist gh logito pcache go-mode handlebars-mode haskell-mode helm-flycheck helm async flycheck let-alist pkg-info epl dash dash highlight-indentation idomenu iedit jabber jinja2-mode js2-mode json-mode json-snatcher json-reformat json-reformat json-snatcher jsx-mode less-css-mode let-alist logito lua-mode magit magit-popup dash git-commit with-editor dash dash with-editor dash magit-popup dash moe-theme nav nose paredit pcache pkg-info epl popup powerline processing-mode pyvenv quack s sass-mode haml-mode scheme-complete scss-mode solarized-theme dash vimrc-mode web-mode with-editor dash noctilux-theme helm-projectile neotree))
 
 ;; Installs missing packages
 (defun install-missing-packages ()
@@ -367,7 +347,7 @@ This command does not push text to `kill-ring'."
 
 (nyan-mode)
 
-(setq sql-mysql-program "/usr/local/bin/mysql")
+(setq sql-mysql-program "/usr/bin/mysql")
 (setq sql-user "root")
 (setq sql-password "")
 (setq sql-server "localhost")
